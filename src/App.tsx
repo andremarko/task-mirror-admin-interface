a00f4f7b6645f0dac40ef47d82ca9b65c2d23ef1
andremarko <151890840+andremarko@users.noreply.github.com>
2025-11-20 22:32:31 -0300

    feat: adiciona routing, context provider, rotas privadas e extendTheme para customizacao do tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
-import Homepage from './pages/Homepage/Homepage';
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
+import React from "react";
+import Homepage from "./pages/Homepage/Homepage";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { theme } from "./theme/theme";
+import Usuarios from "./pages/Usuarios/Usuarios";
+import Login from "./pages/Login/Login";
+import { AuthProvider } from "./context/AuthContext";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
-  <ChakraProvider value={defaultSystem}>
-        <Homepage />
-  </ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <Homepage />
+                </PrivateRoute>
+              }
+            />
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/usuarios"
+              element={
+                <PrivateRoute>
+                  <Usuarios />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </ChakraProvider>
   );
 }
 
